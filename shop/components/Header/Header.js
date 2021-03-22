56a7372dbcf85028c661bdc6d4ea4734268206a3
Alina <74315328+alinatuktarova@users.noreply.github.com>
2021-03-22 18:25:58 +0300

    Update Header.js

--- shop/components/Header/Header.js
+++ shop/components/Header/Header.js
@@ -9,9 +9,9 @@ class Header{
         if (length==-2) l=0; else l=l/2;
         const html=`
             <div class='header-container'>
-                <img src='../images/icons/LOGO1.png' class='header-container__img'/>
+                <img src='./images/icons/LOGO1.png' class='header-container__img'/>
                 <div class='header-counter' onclick='headerPage.handlerOpenShoppingPage()'>
-                    <img src='../images/icons/shop.png' class='header-counter__img'/>
+                    <img src='./images/icons/shop.png' class='header-counter__img'/>
                     <span class='header-counter__count'>(${l})</span>
                 </div>
             </div>
